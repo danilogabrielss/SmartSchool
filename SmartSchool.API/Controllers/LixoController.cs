441449277ff1e2fd1ce7e1e9271bc3329981bd34
Danilo Gabriel <59902770+danilogabrielss@users.noreply.github.com>
2021-03-03 19:56:57 -0300

    aula19

---
+++ SmartSchool.API/Controllers/LixoController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartSchool.API.Controllers
+{
+    public class LixoController : Controller
+    {
+        // GET: LixoController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: LixoController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: LixoController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LixoController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LixoController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: LixoController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LixoController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: LixoController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SmartSchool.WebAPI/Models/Aluno.cs
@@ -0,0 +1,20 @@
+namespace SmartSchool.WebAPI.Models
+{
+    public class Aluno
+    {
+        public Aluno() { }
+        public Aluno(int id, string nome, string sobrenome, int telefone)
+        {
+            this.id = id;
+            this.nome = nome;
+            this.sobrenome = sobrenome;
+            this.telefone = telefone;
+        }
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string sobrenome { get; set; }
+        public int telefone { get; set; }
+        public IEnumerable<AlunoDisciplina> AlunosDisciplinas { get; set; }
+
+    }
+}
