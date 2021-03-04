16c018c8787e9078222a7171d3daf3f13db01093
Danilo Gabriel <59902770+danilogabrielss@users.noreply.github.com>
2021-03-03 23:19:08 -0300

    aula21 incompleta

--- SmartSchool.WebAPI/Controllers/AlunoController.cs
+++ SmartSchool.WebAPI/Controllers/AlunoController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using SmartSchool.WebAPI.Models;
 
 namespace SmartSchool.WebAPI.Models
 {
@@ -6,11 +8,32 @@ namespace SmartSchool.WebAPI.Models
     [Route("api/[controller]")]
     public class AlunoController : ControllerBase
     {
+        public List<Aluno>Alunos = new  List<Aluno>(){
+            new Aluno () {
+                id = 1,
+                nome = "Joao",
+                sobrenome = "Abreu e Lima",
+                telefone = "151515166"
+            },
+            new Aluno () {
+                id = 2,
+                nome = "Marta",
+                sobrenome = "Alves",
+                telefone = "145265897"
+            },
+            new Aluno () {
+                id = 3,
+                nome = "Carlos",
+                sobrenome = "Nobrega",
+                telefone = "559952854"
+            },
+        };
+        
         public AlunoController(){}
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok("Alunos: João e Mário");
+            return Ok(Alunos);
         }
     }
 }
--- SmartSchool.WebAPI/Models/Aluno.cs
+++ SmartSchool.WebAPI/Models/Aluno.cs
@@ -5,7 +5,7 @@ namespace SmartSchool.WebAPI.Models
     public class Aluno
     {
         public Aluno() { }
-        public Aluno(int id, string nome, string sobrenome, int telefone)
+        public Aluno(int id, string nome, string sobrenome, string telefone)
         {
             this.id = id;
             this.nome = nome;
@@ -15,7 +15,7 @@ namespace SmartSchool.WebAPI.Models
         public int id { get; set; }
         public string nome { get; set; }
         public string sobrenome { get; set; }
-        public int telefone { get; set; }
+        public string telefone { get; set; }
         public IEnumerable<AlunoDisciplina> AlunosDisciplinas { get; set; }
     }
 }
