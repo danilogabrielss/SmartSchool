6d83d00da2b6afa56ef2d2ff72e6f0dafee100b7
Danilo Gabriel <59902770+danilogabrielss@users.noreply.github.com>
2021-03-11 18:44:55 -0300

    aula 23 incompleta

--- SmartSchool.WebAPI/Controllers/AlunoController.cs
+++ SmartSchool.WebAPI/Controllers/AlunoController.cs
@@ -13,7 +13,7 @@ namespace SmartSchool.WebAPI.Models
             new Aluno () {
                 Id = 1,
                 Nome = "Joao",
-                Sobrenome = "Abreu e Lima",
+                Sobrenome = "Abreu",
                 Telefone = "151515166"
             },
             new Aluno () {
@@ -32,18 +32,51 @@ namespace SmartSchool.WebAPI.Models
         
         public AlunoController(){}
         [HttpGet]
-        public IActionResult Get()
+        public IActionResult Get(Aluno aluno)
         {
-            return Ok(Alunos);
+            return Ok(aluno);
         }
         
-        [HttpGet("{id}")]
+        [HttpGet("byId")]
         public IActionResult GetById(int id)
         {
-            var Aluno = Alunos.FirstOrDefault(a => a.Id == id);
-            if (Aluno == null) return BadRequest("O aluno não foi encontrado.");
+            var aluno = Alunos.FirstOrDefault(a => a.Id == id);
+            if (aluno == null) return BadRequest("O aluno não foi encontrado.");
 
-            return Ok(Aluno);
+            return Ok(aluno);
+        }
+
+        [HttpGet("byName")]
+        public IActionResult GetByName(string nome, string Sobrenome)
+        {
+            var aluno = Alunos.FirstOrDefault(a => a.Nome.Contains(nome) && a.Sobrenome.Contains(Sobrenome));
+            if (aluno == null) return BadRequest("O aluno não foi encontrado.");
+
+            return Ok(aluno);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult Patch (int id, Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id, Aluno aluno)
+        {
+            return Ok(aluno);
         }
     }
 }
